a75bb08059c665d74cb532ce5635d7e5b8470543
Subrata Mazumder <subrata.besu@gmail.com>
2020-02-29 04:12:01 +0530

    docs added

--- hello.erl
+++ hello.erl
@@ -1,4 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author subratamazumder
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Feb 2020 03:34
+%%%-------------------------------------------------------------------
 -module(hello).
+-author("subratamazumder").
+
+%% API
 -export([double/1]).
 -export([multiply/2]).
 
--- touples.erl
+++ touples.erl
@@ -1,4 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author subratamazumder
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Feb 2020 03:34
+%%%-------------------------------------------------------------------
 -module(touples).
+-author("subratamazumder").
+
+%% API
 -export([convert/1]).
 
 convert({inch, M}) ->
